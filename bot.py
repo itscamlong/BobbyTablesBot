a108a7ef36ec7d30f66356939b784daef4aee315
Cam Long <clong2@umbc.edu>
2020-02-05 17:14:20 -0500

    Init commit

---
+++ bot.py
@@ -0,0 +1,29 @@
+import os
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def xkcd(ctx, num):
+    try: 
+        num = int(num)
+        url = "https://xkcd.com/" + str(num)
+        await ctx.send(url)
+    except ValueError:
+        await ctx.send("Strings are not yet supported.")
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+    print(f'{bot.user.name} is connected to the following guild(s): \n')
+
+    for guild in bot.guilds:
+        print(f'{guild.name} (id: {guild.id})')
+
+bot.run(token)
